8987c3736929a2f2f4926beb062053a3e143661d
Konrad Krawczyk <kk3144@nyu.edu>
2024-12-19 16:57:01 +0100

    include build

---
+++ build/precache-manifest.a2889a2b6603b5d68716d829db535b3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "63d6a4006fe509823e7d",
+    "url": "/static/css/main.93c91547.chunk.css"
+  },
+  {
+    "revision": "63d6a4006fe509823e7d",
+    "url": "/static/js/main.63d6a400.chunk.js"
+  },
+  {
+    "revision": "32aeba456f73a79f0176",
+    "url": "/static/js/1.32aeba45.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a48ac41620cd818c5020d0f4302489ff",
+    "url": "/static/media/RobotoMono-Regular.a48ac416.ttf"
+  },
+  {
+    "revision": "e37be82523395f95fa88409d4a6e7870",
+    "url": "/index.html"
+  }
+];
